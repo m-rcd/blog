74e4bf01d0330d43f9517f3103ab1d766c7133a9
Marianne Rachid <marianne.rachid@gmail.com>
2018-10-22 09:34:18 +0100

    create new article and save to db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,22 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(particle_params)
+    @article.save #save model in db/ will return boolean to say if saved or not
+    redirect_to @article #redirect user to show action
+  end
+
+  private
+  def particle_params
+    params.require(:article).permit(:title, :text)
   end
 end
+
+#A frequent practice is to place the standard CRUD actions in each controller
+#in the following order: index, show, new, edit, create, update and destroy
